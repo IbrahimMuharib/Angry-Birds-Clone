6d179555020d167d7badf666499a271491ced990
IbrahimMuharib <ibrahim.a.muharib@gmail.com>
2024-05-19 06:20:05 +0300

    reset level on lose

--- Scirpts/GameManager.cs
+++ Scirpts/GameManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -81,5 +82,6 @@ public class GameManager : MonoBehaviour
     private void Lose()
     {
         Debug.Log("LOSE");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
